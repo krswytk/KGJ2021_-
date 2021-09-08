53ff569d269a8c27a0bb74f2af0924b2c4b7d057
2123114-PC\2123114 <yuto021025@gmail.com>
2021-09-08 17:34:51 +0900

    タイトルからの移行

---
+++ Assets/FadeScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+    float alfa;
+    float speed = 0.01f;
+    float red, green, blue;
+
+    void Start()
+    {
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+    }
+
+    void Update()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        alfa += speed;
+    }
+}
